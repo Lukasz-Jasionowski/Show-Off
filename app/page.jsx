d9d42a2f3063b461247e4ed9977192eb50987eb3
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-10-09 20:37:44 +0200

    Add content to page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
+import Feed from "@components/Feed"
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">Discover & Share
+                <br className="max-md:hidden" />
+                <span className="green_gradient text-center">Show off</span>
+            </h1>
+            <p className="desc text_center">This is the place where you can boast about your achievements, seek motivation and discover what others have managed to do or are proud of.</p>
+            <Feed />
+        </section>
     )
 }
 
