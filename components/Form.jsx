2d1411814fb824c3a085545aa57905b6f64b147c
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-10-23 21:51:26 +0200

    Remove # from label and input placeholder

--- components/Form.jsx
+++ components/Form.jsx
@@ -21,11 +21,11 @@ const Form = ({
                     />
                 </label>
                 <label>
-                    <span className='font-satoshi font-semibold text-base text-gray-700'>Tag<span className='font-normal'> (#product, #webdevelopment, #idea, #running)</span></span>
+                    <span className='font-satoshi font-semibold text-base text-gray-700'>Tag<span className='font-normal'> (product, webdevelopment, idea, running)</span></span>
                     <input
                         value={post.tag}
                         onChange={(e) => setPost({ ...post, tag: e.target.value })}
-                        placeholder='#tag'
+                        placeholder='tag'
                         required
                         className='form_input'
                     />
